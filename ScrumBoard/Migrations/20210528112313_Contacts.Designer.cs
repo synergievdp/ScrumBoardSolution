d53e4dca4202816ef11065eba16419daa6186687
Sam Teeuwisse <sam.teeuwisse2@gmail.com>
2021-05-28 14:36:48 +0200

    Patch database table name

--- ScrumBoard/Migrations/20210528112313_Contacts.Designer.cs
+++ ScrumBoard/Migrations/20210528112313_Contacts.Designer.cs
@@ -3,15 +3,17 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using ScrumBoard.Services;
 
 namespace ScrumBoard.Migrations
 {
     [DbContext(typeof(ScrumBoardDbContext))]
-    partial class ScrumBoardDbContextModelSnapshot : ModelSnapshot
+    [Migration("20210528112313_Contacts")]
+    partial class Contacts
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ ScrumBoard/Migrations/20210528112313_Contacts.cs
@@ -0,0 +1,63 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace ScrumBoard.Migrations
+{
+    public partial class Contacts : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_ToDos_Contact_ContactId",
+                table: "ToDos");
+
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_Contact",
+                table: "Contact");
+
+            migrationBuilder.RenameTable(
+                name: "Contact",
+                newName: "Contacts");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_Contacts",
+                table: "Contacts",
+                column: "Id");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_ToDos_Contacts_ContactId",
+                table: "ToDos",
+                column: "ContactId",
+                principalTable: "Contacts",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Restrict);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_ToDos_Contacts_ContactId",
+                table: "ToDos");
+
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_Contacts",
+                table: "Contacts");
+
+            migrationBuilder.RenameTable(
+                name: "Contacts",
+                newName: "Contact");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_Contact",
+                table: "Contact",
+                column: "Id");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_ToDos_Contact_ContactId",
+                table: "ToDos",
+                column: "ContactId",
+                principalTable: "Contact",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Restrict);
+        }
+    }
+}
